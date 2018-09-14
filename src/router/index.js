c0c0685140dcc391a3804f6be17df3f1957ce38a
Zuolar <yam8511@gmail.com>
2018-09-15 02:22:34 +0800

    基本的CRUD

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/views/HelloWorld'
+import TodoList from '@/views/TodoList'
 
 Vue.use(Router)
 
@@ -13,7 +14,7 @@ export default new Router({
     },
     {
       path: '/todo-list',
-      component: HelloWorld
+      component: TodoList
     }
   ]
 })
--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,9 +1,165 @@
 <template>
-    <div></div>
+  <div class="hello">
+    <img src="../assets/logo.png">
+    <!-- 本體 -->
+    <el-card class="box-card" shadow="hover">
+      <!-- 標題區 -->
+      <div slot="header" class="clearfix">
+        <div class="card-header">任務清單</div>
+        <!-- 輸入塊 -->
+        <input
+          class="text"
+          placeholder="請輸入新任務"
+          v-model="newTodo"
+          @keyup.enter="addTodo"
+        />
+        <!-- 輸入按鈕 -->
+        <el-button
+          type="warning"
+          icon="el-icon-circle-plus-outline"
+          @click="addTodo"
+          plain
+        >
+        </el-button>
+      </div>
+      <!-- 列表區 -->
+      <div v-for="todo in todos" :key="todo.id" class="text item">
+        <el-checkbox v-model="todo.done">
+          <!-- 顯示完成任務 -->
+          <span v-if="todo.done">
+            <del class="text done">{{ todo.text }}</del>
+          </span>
+          <!-- 顯示未完成任務 -->
+          <span v-else class="text">{{ todo.text }}</span>
+          <!-- 修改按鈕 -->
+          <el-button
+            type="success"
+            icon="el-icon-edit"
+            @click="editTodo(todo)"
+            plain
+          >
+          </el-button>
+          <!-- 刪除按鈕 -->
+          <el-button
+            type="danger"
+            icon="el-icon-delete"
+            plain
+            @click="deleteTodo(todo)"
+          ></el-button>
+        </el-checkbox>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
+  // 資料區
+  data () {
+    return {
+      newTodo: '',
+      filter: '0',
+      todos: [{
+        id: 1,
+        text: 'Golang',
+        done: true
+      }, {
+        id: 2,
+        text: 'Docker',
+        done: false
+      }],
+      id: 0
+    }
+  },
+  // 設定給 template 用的，具有快取作用
+  computed: {
+    filterTodos: function () {
+      let filterTodos = this.todos
+      if (this.filter === '1') {
+        filterTodos = filterTodos.filter((todo) => {
+          return todo.done === false
+        })
+      } else if (this.filter === '2') {
+        filterTodos = filterTodos.filter((todo) => {
+          return todo.done === true
+        })
+      }
+      return filterTodos
+    }
+  },
+  // 監聽變數
+  watch: {
+    newTodo: function () {
+      console.log('新任務有改變,', this.newTodo)
+    }
+  },
+  // 方法
+  methods: {
+    addTodo () {
+      this.newTodo = this.newTodo.trim()
+      if (this.newTodo === '') {
+        return
+      }
+      console.log('增加新任務', this.newTodo)
+      this.todos.push({
+        id: this.id++,
+        text: this.newTodo,
+        done: false
+      })
+      this.newTodo = ''
+    },
+    editTodo (editTodo) {
+      let newText = prompt('請輸入要修改的任務')
+      newText = newText.trim()
+      if (newText === '') {
+        return
+      }
+      this.todos = this.todos.filter((todo) => {
+        if (todo.id === editTodo.id) {
+          todo.text = newText
+        }
+        return todo
+      })
+    },
+    deleteTodo (deletedTodo) {
+      this.todos = this.todos.filter((todo) => {
+        return todo !== deletedTodo
+      })
+    }
+  },
+  mounted () {
+    this.id = this.todos.length + 1
+    console.log('掛載完了, 目前ID =', this.id)
+  }
 }
 </script>
+
+<style scoped>
+  .card-header {
+    font-size: 36px
+  }
+  .text {
+    font-size: 24px;
+    color: #333;
+  }
+  .done {
+    color: #AAA;
+  }
+  .item {
+    margin-bottom: 18px;
+  }
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+  .box-card {
+    margin: auto;
+    width: 50%;
+    border: 3px solid green;
+    padding: 10px;
+  }
+</style>
